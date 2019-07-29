40da04991741fd6b6f1a03d827d06a6ba8c6f1d6
Shoaib Hamza <shoaibhamza896@gmail.com>
2019-07-29 15:32:08 +0530

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,7 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var data = [];
 module.exports = function(app){
     
     app.get('/StaHotel', function(req,res){
@@ -7,8 +11,19 @@ module.exports = function(app){
     app.get('/Form',function(req,res){
        res.render('Form'); 
     });
-     app.post('/StaHotel', function(req,res){
-        
+    app.post('/Form', urlencodedParser, function(req,res){
+       data.push(req.body);
+       res.json(data);
     });
-    
+    app.get('/myOrder',function(req,res){
+        
+            res.render('myOrder', {orders : data});
+         
+    });
+    app.delete('/myOrder/:item',function(req,res){
+       data = data.filter(function(myOrder){
+           return myOrder.item !== req.params.item;
+       }) ;
+        res.json(data);
+    });
 };
