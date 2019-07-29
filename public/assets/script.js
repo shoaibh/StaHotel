ae40f48ad1f39f2de8db0ea493fbfd559f7509de
Shoaib Hamza <shoaibhamza896@gmail.com>
2019-07-29 15:33:10 +0530

    Add files via upload

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,12 +1,11 @@
 
-//var x =0;
-//
-var last = $(window).scrollTop();
- 
-
-
-
-var l = $(window).scroll(function(){
+$(document).ready(function(){
+    if(window.location.href === 'http://localhost:3000/myOrder'){
+       $('.home').removeClass('active');
+        $('.myOrders').addClass('active');
+       }
+     else{var last = $(window).scrollTop();
+ var l = $(window).scroll(function(){
     var last = $(window).scrollTop();
     if(last>300){
      $('.main').addClass('blur');
@@ -20,6 +19,36 @@ if(last==0){
     $('.home').addClass('active'); 
         $('.nmenu').removeClass('active');
     }
+});}
+
+$('form').on('submit', function(){
+    var item = $('#order');
+    var name = $('#name')
+    var orders = {item: item.val(), name: name.val()};
+    
+    $.ajax({
+        type: 'POST',
+        url: '/Form',
+        data: orders,
+        success: function(data){
+            location.assign('/myOrder')
+        }
+    });
+    return false;
 });
+    
+    $('.delete').on('click',function(){
+        console.log('helo')
+        var item = $(this)[0].parentElement.firstChild.innerText
+        console.log(item)
+        $.ajax({
+            type:'DELETE',
+            url:'/myOrder/'+item,
+            success: function(data){
+                location.reload();
+            }
+        });
+        return false;
+    });
 
-
+});
