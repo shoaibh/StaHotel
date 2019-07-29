5139a95594fba68e9da80e266b7f6e14abe0b946
Shoaib Hamza <shoaibhamza896@gmail.com>
2019-07-29 15:34:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var controller = require('./controllers/controller')
 app.set('view engine','ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
 
 app.use(express.static('./public'));
 
 controller(app);
 
 app.listen(3000);
-
-console.log('hello');
